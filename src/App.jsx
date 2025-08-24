f1292b98cab98e5528b57c58c518536177183c97
RimgaudasGreenhat <rimgaudasAER@gmail.com>
2025-08-24 20:31:52 +0300

    backroundTitleAndSearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+import Search from "./components/Search.jsx";
+
 export default function App()
 {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return(
     <main className="w-full p-2 flex flex-col items-center">
       <div className="flex items-center">
-        <h1>Game Database</h1>
-        <img src="./logo.png" className="w-25 h-25" /> 
+        <h1 className="text-gradient text-4xl font-bold">Game Database</h1>
+        <img src="./logo.png" className="w-25 h-25" />
       </div>
+      <div className="w-full max-w-4xl">
+          <Search
+          query = {searchQuery}
+          onChange= {(e) => setSearchQuery(e.target.value)}
+          />
+          </div> 
     </main>
   );
 }
