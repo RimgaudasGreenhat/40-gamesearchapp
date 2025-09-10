d75a6a823717b828d352c23eae2b84a45eeb44d6
RimgaudasGreenhat <rimgaudasAER@gmail.com>
2025-09-10 23:30:27 +0300

    gamecards_generalCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { useState } from "react";
 import Search from "./components/Search.jsx";
+import { useEffect } from "react";
+import useFetch from "./hooks/useFetch.js";
+import { fetchGames } from "./api.js";
+import GameCard from "./components/GameCard.jsx";
 
 export default function App()
 {
   const [searchQuery, setSearchQuery] = useState("");
+
+  const { data, loading, fetchData, error, reset } = useFetch(() =>
+    fetchGames({ query: searchQuery })
+  );
+
+  useEffect(() => {
+    const timeoutId = setTimeout(async () => {
+      if (searchQuery.trim()) {
+        await fetchData();
+        return;
+      }
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
+
+  useEffect(() => {
+    if (data) console.log(data)
+  }, [data]);
 
   return(
     <main className="w-full p-2 flex flex-col items-center">
@@ -16,10 +39,33 @@ export default function App()
           query = {searchQuery}
           onChange= {(e) => setSearchQuery(e.target.value)}
           />
-          </div> 
+          <div className="w-full">
+            {data && (
+              <div className="grid grid-cols-1 gap-5 sm:grid-cols-2
+              lg:grid-cols-3">
+                {data.results.map(
+                (game) => 
+                  game.added > 30 && (
+                    <GameCard
+                    key={game.slug}
+                    name={game.name}
+                    coverLink={game.backround_image}
+                    playtime={game.playtime}
+                    genres={game.genres}
+                    onClick={() => {
+                      console.log("test");
+                      
+                    }}
+                    />
+                  )
+                )}
+            </div>
+            )}
+        </div>
+    </div> 
     </main>
   );
 }
 
 
-//pratest nuo 1:00:00 :)
+//pratest nuo 1:08:27 :)
