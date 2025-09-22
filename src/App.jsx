f84fc5410ba6096dda4ce7677e359069361808ea
RimgaudasGreenhat <rimgaudasAER@gmail.com>
2025-09-23 00:28:14 +0300

    paveiksliukuKaruseleIrZaidimuDuomenysReikSudetPointeriusAntElementu

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import Search from "./components/Search.jsx";
-import { useEffect } from "react";
-import useFetch from "./hooks/useFetch.js";
-import { fetchGames } from "./api.js";
-import GameCard from "./components/GameCard.jsx";
-import ErrorMessage from "./components/ErrorMessage.jsx";
-import Spinner from "./components/Spinner.jsx";
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
+import GameCard from "./components/GameCard";
+import useFetch from "./hooks/useFetch";
+import { fetchGames } from "./api";
+import Spinner from "./components/Spinner";
+import GameDetails from "./components/GameDetails";
 
-export default function App()
-{
+function App() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [showGameDetails, setShowGameDetails] = useState(false);
+  const [currentGame, setCurrentGame] = useState(null);
 
   const { data, loading, fetchData, error, reset } = useFetch(() =>
     fetchGames({ query: searchQuery })
@@ -18,66 +18,74 @@ export default function App()
   useEffect(() => {
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
+        setShowGameDetails(false);
+        setCurrentGame(null);
         await fetchData();
-        return;
       }
     }, 500);
 
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
 
-  useEffect(() => {
-    if (data) console.log(data)
-  }, [data]);
-
-  return(
+  return (
     <main className="w-full p-2 flex flex-col items-center">
       <div className="flex items-center">
         <h1 className="text-gradient text-4xl font-bold">Game Database</h1>
         <img src="./logo.png" className="w-25 h-25" />
       </div>
       <div className="w-full max-w-4xl">
-          <Search
-          query = {searchQuery}
-          onChange= {(e) => setSearchQuery(e.target.value)}
-          />
-          <div className="w-full">
-            {!loading? (
-              <>
-                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2
-              lg:grid-cols-3">
-                {data.results.map(
-                (game) => 
-                  game.added > 30 && (
-                    <GameCard
-                    key={game.slug}
-                    name={game.name}
-                    coverLink={game.background_image}
-                    playtime={game.playtime}
-                    genres={game.genres}
-                    onClick={() => {
-                      console.log("test"); 
-                    }}
-                    />
-                  )
-                )}
-              </div>
-              {data?.results.length === 0 && (
-              <ErrorMessage message={"No games were found sadge :("} />
-            )}
-            </>  
-            ) : (
-             <div className="flex justify-center">
+        <Search
+          query={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="w-full">
+          {!loading && !showGameDetails ? (
+            <>
+              {data && (
+                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+                  {data.results.map(
+                    (game) =>
+                      game.added > 30 && (
+                        <GameCard
+                          key={game.slug}
+                          name={game.name}
+                          coverLink={game.background_image}
+                          playtime={game.playtime}
+                          genres={game.genres}
+                          onClick={() => {
+                            setCurrentGame(game);
+                            setShowGameDetails(true);
+                          }}
+                        />
+                      )
+                  )}
+                </div>
+              )}
+
+              {data && data.results.length === 0 && (
+                <div className="w-full  flex flex-col justify-center items-center">
+                  <img src="./no-results-found.png" className="size-52" />
+                  <p className="text-gray-300 text-2xl">No games found!</p>
+                </div>
+              )}
+            </>
+          ) : showGameDetails ? (
+            <GameDetails
+              currentGame={currentGame}
+              goBack={() => {
+                setShowGameDetails(false);
+                setCurrentGame(null);
+              }}
+            />
+          ) : (
+            <div className="flex justify-center">
               <Spinner />
-             </div> 
-            )}
-            {error && <ErrorMessage message={"Network Error Occured!"} />}
-          
+            </div>
+          )}
         </div>
-    </div> 
+      </div>
     </main>
   );
 }
 
-
-//pratest nuo 1:25:18 :)
+export default App;
--- src/api.js
+++ src/api.js
@@ -4,3 +4,8 @@ export async function fetchGames({ query }) {
   const response = await fetch(`${API_ENDPOINT}/games?search=${query}`);
   return await response.json();
 }
+
+export async function fetchGameData({ gameSlug }) {
+  const response = await fetch(`${API_ENDPOINT}/game?gameSlug=${gameSlug}`);
+  return await response.json();
+}
